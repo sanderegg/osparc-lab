418d6741a459d2906eec828a8c5e5ddc3fe39fae
odeimaiz <maiz@zurichmedtech.com>
2018-01-24 17:12:41 +0100

    Hallo World

---
+++ demos/qooxdoo-plotly/app/source/class/app/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright: 2018 
+
+   License: MIT license
+
+   Authors: 
+
+************************************************************************ */
+
+qx.Theme.define("app.theme.Theme",
+{
+  meta :
+  {
+    color : app.theme.Color,
+    decoration : app.theme.Decoration,
+    font : app.theme.Font,
+    icon : qx.theme.icon.Oxygen,
+    appearance : app.theme.Appearance
+  }
+});
